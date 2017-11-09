c491f0a8140e9951f143dea2dac760d12bf36fcb
monkingxue <1360662499@qq.com>
2017-11-09 19:22:03 +0800

    [feat] add sticky

---
+++ doc/README.md
@@ -0,0 +1,19 @@
+# ts-dva
+
+A demo project builded by typescript + react + webpack + antd-mobile.
+
+## How to use
+
+First, at root of project in ternimal.
+
+``` bash
+npm install
+```
+
+After installed successfully, input
+
+``` bash
+npm start
+```
+
+code will auto open you broswer, please change broswer to mobile mode, because this project is a mobile web application.
--- src/TechNews.tsx
+++ src/TechNews.tsx
@@ -1,9 +1,16 @@
 import { Badge, Tabs } from 'antd-mobile';
 import React from 'react';
+import { Sticky, StickyContainer } from 'react-sticky';
 
 import Content from './Content';
 
 const category = [['技术', 300], ['创意', 17], ['好玩', 16], ['酷工作', 43], ['交易', 69], ['城市', 18], ['问与答', 12]];
+
+const renderTabBar = (props: any) => {
+	return (<Sticky>
+		{({ style }: any) => <div style={{ ...style, zIndex: 1 }}><Tabs.DefaultTabBar {...props} /></div>}
+	</Sticky>);
+};
 
 export default class TechNews extends React.Component<{}, {}> {
 
@@ -13,11 +20,15 @@ export default class TechNews extends React.Component<{}, {}> {
 		});
 
 		return (
-			<Tabs
-				tabs={tabs}
-				initialPage={0}>
-				{category.map(([_, id]: [string, number]) => <Content key={id} nodeId={id} />)}
-			</Tabs>
+			<StickyContainer>
+				<Tabs
+					tabs={tabs}
+					initialPage={0}
+					renderTabBar={renderTabBar}
+				>
+					{category.map(([_, id]: [string, number]) => <Content key={id} nodeId={id} />)}
+				</Tabs>
+			</StickyContainer>
 		);
 	}
 }
